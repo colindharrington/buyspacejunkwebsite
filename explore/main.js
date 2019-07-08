b89687498748959c8f6e3f889bf7070b59e98a40
colindharrington <harrc386@newschool.edu>
2019-07-08 04:43:22 -0400

    f

--- explore/main.js
+++ explore/main.js
@@ -44,3 +44,8 @@ $(function() {
     $('.submenu').slideToggle();
   });
 });
+
+var ul = document.querySelector('ul');
+for (var i = ul.children.length; i >= 0; i--) {
+    ul.appendChild(ul.children[Math.random() * i | 0]);
+}
